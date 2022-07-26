8babb1da2aa6f42f1c2311e14522c94a338cf98b
fetails <vikerdeflike@gmail.com>
2022-07-26 20:36:51 +0200

    d

--- src/graphics/renderer/renderer.cpp
+++ src/graphics/renderer/renderer.cpp
@@ -4,8 +4,11 @@
 #include <glad/glad.h>
 #include <glm/gtc/matrix_transform.hpp> // ortho
 #include "../../src/graphics/shaders/shader/shader.hpp"
-#include "vertexbuffer.hpp"
+
 #include "vertexarray.hpp"
+#include "vertexbuffer.hpp"
+#include "indexbuffer.hpp"
+#include "framebuffer.hpp"
 
 constexpr uint32_t max_quad_count = 32000;
 constexpr uint32_t max_vertices_count = max_quad_count * 4;
@@ -29,75 +32,30 @@ struct RendererData {
     glm::vec4 m_QuadTransform[ 4 ];
     glm::vec2 m_TextureTransform[ 4 ];
 
-    Viper::Ref<Viper::Renderer::VertexArray> QuadVAO;
+    Viper::Ref<Viper::Renderer::VertexArray> VertexArray;
+    Viper::Ref<Viper::Renderer::IndexBuffer> IndexBuffer;
+    Viper::Ref<Viper::Renderer::VertexBuffer> VertexBuffer;
+    Viper::Ref<Viper::Renderer::FrameBuffer> FrameBuffer;
 };
 
 RendererData s_Renderer;
 namespace Viper::Renderer {
     Renderer2D::Renderer2D()
     {
-        m_Vao = 0;
-        m_Vbo = 0;
-        m_Ibo = 0;
-        m_Fbo = 0;
-        m_Tcb = 0;
-        m_TestTexture = 0;
-
         s_Renderer.m_VertexBuffer = new Viper::RendererAPI::Vertex_t[ max_quad_count ];
         
-        //glGenVertexArrays(1, &m_Vao);
-        //glBindVertexArray(m_Vao);
-        s_Renderer.QuadVAO = Viper::Renderer::VertexArray::Create();
-        s_Renderer.QuadVAO->Bind();
+        s_Renderer.VertexArray = VertexArray::Create();
+        s_Renderer.VertexArray->Bind();
 
-        glGenBuffers(1, &m_Vbo);
-        glBindBuffer(GL_ARRAY_BUFFER, m_Vbo);
-        glBufferData(GL_ARRAY_BUFFER, max_vertices_count * sizeof(RendererAPI::Vertex_t), nullptr, GL_DYNAMIC_DRAW);
-    
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof( RendererAPI::Vertex_t), ( const void* )offsetof(RendererAPI::Vertex_t, position));
+        s_Renderer.VertexBuffer = VertexBuffer::Create(max_vertices_count * sizeof(RendererAPI::Vertex_t));
+        s_Renderer.VertexBuffer->Bind();
+        
+        s_Renderer.IndexBuffer = IndexBuffer::Create(max_indices_count);
+        s_Renderer.IndexBuffer->Bind();
 
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof( RendererAPI::Vertex_t), ( const void* )offsetof(RendererAPI::Vertex_t, color));
+        s_Renderer.VertexArray->Unbind();
 
-        glEnableVertexAttribArray(2);
-        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof( RendererAPI::Vertex_t), ( const void* )offsetof(RendererAPI::Vertex_t, texcoords));
-
-        glEnableVertexAttribArray(3);
-        glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, sizeof( RendererAPI::Vertex_t), ( const void* )offsetof(RendererAPI::Vertex_t, texindex));
-    
-        uint32_t m_IndexArray[ max_indices_count ];
-        uint32_t m_IndexOffset = 0;
-
-        for( size_t index = 0; index < max_indices_count; index += 6 ) {
-            m_IndexArray[ index + 0 ] = 0 + m_IndexOffset;
-            m_IndexArray[ index + 1 ] = 1 + m_IndexOffset;
-            m_IndexArray[ index + 2 ] = 2 + m_IndexOffset;
-            m_IndexArray[ index + 3 ] = 2 + m_IndexOffset;
-            m_IndexArray[ index + 4 ] = 3 + m_IndexOffset;
-            m_IndexArray[ index + 5 ] = 0 + m_IndexOffset;
-
-            m_IndexOffset += 4;
-        };
-
-        glGenBuffers(1, &m_Ibo);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_Ibo);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(m_IndexArray), m_IndexArray, GL_STATIC_DRAW);
-
-        s_Renderer.QuadVAO->Unbind();
-
-        glGenFramebuffers(1, &m_Fbo);
-        glBindFramebuffer(GL_FRAMEBUFFER, m_Fbo);
-
-        // TESTING. (Skapa textur.)
-        glGenTextures(1, &m_Tcb);
-        glBindTexture(GL_TEXTURE_2D, m_Tcb);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 1280, 720, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glBindTexture(GL_TEXTURE_2D, 0);
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_Tcb, 0);
-        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        s_Renderer.FrameBuffer = FrameBuffer::Create();
 
         s_Renderer.m_QuadShader = Graphics::Shader::Create("resources/flat.vert", "resources/flat.frag");
         s_Renderer.m_TextureShader = Graphics::Shader::Create("resources/quad.vert", "resources/quad.frag");
@@ -116,103 +74,59 @@ namespace Viper::Renderer {
     Renderer2D::~Renderer2D()
     {
         delete[] s_Renderer.m_VertexBuffer;
-        glDeleteFramebuffers(1, &m_Fbo);
-        glDeleteVertexArrays(1, &m_Vao);
-        glDeleteTextures(1, &m_Tcb);
-        glDeleteTextures(1, &m_TestTexture);
-        glDeleteBuffers(1, &m_Vbo);
-        glDeleteBuffers(1, &m_Ibo);
     }
 
     void Renderer2D::Flush()
     {
         GLsizeiptr size_ptr = reinterpret_cast< uint8_t* >( s_Renderer.m_VertexBufferPtr ) - reinterpret_cast< uint8_t* >( s_Renderer.m_VertexBuffer );
-        glBindBuffer(GL_ARRAY_BUFFER, m_Vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, s_Renderer.VertexBuffer->Get());
         glBufferSubData(GL_ARRAY_BUFFER, 0, size_ptr, s_Renderer.m_VertexBuffer );
     }
 
     void Renderer2D::ResizeFBO( int Width, int Height ) {
-        RenderCommand::ResizeTexture( m_Tcb, Width, Height );
+        RenderCommand::ResizeTexture( s_Renderer.FrameBuffer->GetTex(), Width, Height );
     }
 
-    void Renderer2D::DrawQuad( const glm::vec2& pos, RendererAPI::Color color ) {
-        auto m_Transform = glm::translate(glm::mat4(1.0f), glm::vec3(pos.x, pos.y, 0.0f)) 
-            * glm::scale(glm::mat4(1.0f), { 1.0f, 1.0f, 1.0f } );
-            
+
+    void Renderer2D::DrawQuad( const glm::mat4& transform, RendererAPI::Color color ) {
         for( int i = 0; i < 4; i++ ) {
-            s_Renderer.m_VertexBufferPtr->position = m_Transform * s_Renderer.m_QuadTransform[ i ];
+            s_Renderer.m_VertexBufferPtr->position = transform * s_Renderer.m_QuadTransform[ i ];
             s_Renderer.m_VertexBufferPtr->color = color;
             s_Renderer.m_VertexBufferPtr->texcoords = s_Renderer.m_TextureTransform[ i ];
             s_Renderer.m_VertexBufferPtr->texindex = ( float )0;
             s_Renderer.m_VertexBufferPtr++;
         };
-
         s_Renderer.m_IndexCount += 6;
         s_Renderer.m_QuadCount++;
     };
-    
+
+    void Renderer2D::DrawQuad( const glm::vec2& pos, RendererAPI::Color color ) {
+            auto m_Transform = glm::translate(glm::mat4(1.0f), glm::vec3(pos.x, pos.y, 0.0f)) 
+             * glm::scale(glm::mat4(1.0f), { 1.0f, 1.0f, 1.0f } );
+
+        DrawQuad(m_Transform, color);
+    };
     void Renderer2D::DrawQuad( const glm::vec2& pos, const glm::vec2& size, RendererAPI::Color color ) {
         auto m_Transform = glm::translate(glm::mat4(1.0f), glm::vec3(pos.x, pos.y, 0.0f)) 
-            * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f } );
+             * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f } );
 
-        for( int i = 0; i < 4; i++ ) {
-            s_Renderer.m_VertexBufferPtr->position = m_Transform * s_Renderer.m_QuadTransform[ i ];
-            s_Renderer.m_VertexBufferPtr->color = color;
-            s_Renderer.m_VertexBufferPtr->texcoords = s_Renderer.m_TextureTransform[ i ];
-            s_Renderer.m_VertexBufferPtr->texindex = ( float )0;
-            s_Renderer.m_VertexBufferPtr++;
-        };
-
-        s_Renderer.m_IndexCount += 6;
-        s_Renderer.m_QuadCount++;
+        DrawQuad(m_Transform, color);
     };
 
     void Renderer2D::DrawQuadRotated( const glm::vec2& pos, float radians, RendererAPI::Color color ) {
         auto m_Transform = glm::translate(glm::mat4(1.0f), glm::vec3(pos.x, pos.y, 0.0f)) 
-            * glm::rotate(glm::mat4(1.0f), radians, glm::vec3(0.0f, 0.0f, 1.0f))
+            * glm::rotate(glm::mat4(1.0f), radians, { 0.0f,0.0f,1.0f } )
             * glm::scale(glm::mat4(1.0f), { 1.0f, 1.0f, 1.0f } );
-        for( int i = 0; i < 4; i++ ) {
-            s_Renderer.m_VertexBufferPtr->position = m_Transform * s_Renderer.m_QuadTransform[ i ];
-            s_Renderer.m_VertexBufferPtr->color = color;
-            s_Renderer.m_VertexBufferPtr->texcoords = s_Renderer.m_TextureTransform[ i ];
-            s_Renderer.m_VertexBufferPtr->texindex = ( float )0;
-            s_Renderer.m_VertexBufferPtr++;
-        }
-        s_Renderer.m_IndexCount += 6;
-        s_Renderer.m_QuadCount++;
+        
+        DrawQuad(m_Transform, color);
     };
 
     void Renderer2D::DrawQuadRotated( const glm::vec2& pos, const glm::vec2& size, float radians, RendererAPI::Color color ) {
         auto m_Transform = glm::translate(glm::mat4(1.0f), glm::vec3(pos.x, pos.y, 0.0f)) 
-            * glm::rotate(glm::mat4(1.0f), radians, glm::vec3(0.0f, 0.0f, 1.0f))
+            * glm::rotate(glm::mat4(1.0f), radians, { 0.0f,0.0f,1.0f } )
             * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f } );
-            
-        for( int i = 0; i < 4; i++ ) {
-            s_Renderer.m_VertexBufferPtr->position = m_Transform * s_Renderer.m_QuadTransform[ i ];
-            s_Renderer.m_VertexBufferPtr->color = color;
-            s_Renderer.m_VertexBufferPtr->texcoords = s_Renderer.m_TextureTransform[ i ];
-            s_Renderer.m_VertexBufferPtr->texindex = ( float )0;
-            s_Renderer.m_VertexBufferPtr++;
-        }
-        s_Renderer.m_IndexCount += 6;
-        s_Renderer.m_QuadCount++;
-    };
-
-    void Renderer2D::DrawTexture( const glm::vec2& pos, uint32_t textureid ) {
-        const auto preset_color = RendererAPI::Color(1.0f,1.0f,1.0f,1.0f);
-
-        auto m_Transform = glm::translate(glm::mat4(1.0f), glm::vec3(pos.x, pos.y, 0.0f)) 
-            * glm::scale(glm::mat4(1.0f), { 1.0f, 1.0f, 1.0f } );
-
-        for( int i = 0; i < 4; i++ ) {
-            s_Renderer.m_VertexBufferPtr->position = m_Transform * s_Renderer.m_QuadTransform[ i ];
-            s_Renderer.m_VertexBufferPtr->color = preset_color;
-            s_Renderer.m_VertexBufferPtr->texcoords = s_Renderer.m_TextureTransform[i];
-            s_Renderer.m_VertexBufferPtr->texindex = ( float )0;
-            s_Renderer.m_VertexBufferPtr++;
-        }
-        s_Renderer.m_IndexCount += 6;
-        s_Renderer.m_QuadCount++;
+        
+        DrawQuad(m_Transform, color);
     };
 
     void Renderer2D::Begin( const OrthoGraphicCamera& camera ) {
@@ -226,10 +140,14 @@ namespace Viper::Renderer {
         s_Renderer.m_QuadShader->SetUniformMat4("u_Transform", m_Transform);
     };
 
+    uint32_t Renderer2D::GetTexture() {
+        return s_Renderer.FrameBuffer->Get();
+    };
+
     void Renderer2D::End() {
 
-        glBindTexture(GL_TEXTURE_2D, m_Tcb);
-        RenderCommand::DrawIndexed(s_Renderer.QuadVAO->Get(), s_Renderer.m_IndexCount );
+        glBindTexture(GL_TEXTURE_2D, s_Renderer.FrameBuffer->GetTex());
+        RenderCommand::DrawIndexed(s_Renderer.VertexArray->Get(), s_Renderer.m_IndexCount );
 
         s_Renderer.m_IndexCount = 0;
         s_Renderer.m_QuadCount = 0;
@@ -237,7 +155,7 @@ namespace Viper::Renderer {
     };
 
     void Renderer2D::BindFramebuffer() {
-        RenderCommand::BindFramebuffer(m_Fbo);
+        RenderCommand::BindFramebuffer(s_Renderer.FrameBuffer->Get());
     };
 
     void Renderer2D::UnbindFramebuffer() {
@@ -279,5 +197,9 @@ namespace Viper::Renderer {
         glBindTexture(GL_TEXTURE_2D, TextureID);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, Width, Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
         glBindTexture(GL_TEXTURE_2D, 0);
+    }
+
+    void RenderCommand::Clear() {
+        glClear(GL_COLOR_BUFFER_BIT);
     };
 };
