b8ea2d5fbeb0a4f49bbe8516d855fc0e28b5bb4f
fetails <vikerdeflike@gmail.com>
2022-07-26 20:37:31 +0200

    FrameBuffer,VertexBuffer,IndexBuffer,VertexArray

---
+++ src/graphics/renderer/framebuffer.hpp
@@ -0,0 +1,29 @@
+//
+// Created by guwi on 2022-07-26.
+//
+
+#ifndef VIPER_FRAMEBUFFER_HPP
+#define VIPER_FRAMEBUFFER_HPP
+#include <cstdint>
+#include "../../util/ref/reference.hpp"
+
+namespace Viper::Renderer {
+    class FrameBuffer {
+    public:
+        FrameBuffer( );
+        ~FrameBuffer();
+
+        uint32_t Get() const;
+        uint32_t GetTex() const;
+
+        void Bind();
+        void Unbind();
+
+        static Ref< FrameBuffer > Create( );
+    private:
+        uint32_t FBO;
+        uint32_t TEX;
+    };
+};
+
+#endif //VIPER_FRAMEBUFFER_HPP
